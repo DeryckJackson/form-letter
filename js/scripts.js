c1fcd0f51e8ddbda212f95122193a089a95be5b5
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-03 09:57:53 -0700

    add logic to amend name to form and show
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  
+  $("#form").submit(function (event) {
+    const personInput = $("input#person").val();
+
+    $(".person").append(personInput);
+
+    $("#hidden").show();
+
+    event.preventDefault();
+  })
 });
